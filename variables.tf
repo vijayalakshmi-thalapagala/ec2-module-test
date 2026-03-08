819c6fe1676aaf081537794f955a514b8ac9eca2
Vijayalakshmi Thalapagala <thalapav@amdocs.com>
2026-03-08 19:19:55 +0530

    actions

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "env" {
+    default = "dev"
+}
+
+variable "sg_ids" {
+    default = ["sg-098e1099d0d2d450d"]
+}
+
+variable "component" {
+    default = "catalogue"
+}
